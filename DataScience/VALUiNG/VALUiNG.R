415c17d51824a7d0a662e737c392ec47158c8480
Navtej S <singhn27@users.noreply.github.com>
2015-02-19 00:16:30 -0500

    Update VALUiNG.R

--- DataScience/VALUiNG/VALUiNG.R
+++ DataScience/VALUiNG/VALUiNG.R
@@ -1,4 +1,4 @@
-# VALUiNG - Veterans Association Lung Cancer Data Analysis using Bayesian Networks
+# VALUiNG - Veterans Affairs Lung Cancer Data Analysis using Bayesian Networks
 # Data provided by http://biostat.mc.vanderbilt.edu/wiki/pub/Main/DataSets/valung.csv
 # Input:           http://biostat.mc.vanderbilt.edu/wiki/pub/Main/DataSets/Cvalung.html (description)
 # Output:          Bayesian network graph, analysis of conditional probabilities, plots
